bed7b74ee2b3f2fcba33f12d8f31b38e73186d06
Skee <skee66499@Gmail.com>
2016-07-07 09:29:03 -0400

    banking updated

--- banking.c
+++ banking.c
@@ -1,8 +1,8 @@
 /*Sean Kee*/
-/*Banking using structures*/
+/*Banking using structures v1.0.12*/
 #include <stdio.h>
 #include <string.h>
-int a = 0; //Global Account Counter
+int a = 0; /*Global Account Counter, states the next available account number for creation*/
 
 typedef struct bankAccount{
 	int number;
@@ -10,14 +10,14 @@ typedef struct bankAccount{
 	float balance;
 } account;
 
-void title() {
+void title() { /*Prints Header*/
 		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		printf("\n^-------------^\n");
 		printf("T KEE BANKING T\n");
 		printf("I_@**_***_**@_I\n\n\n\n");
 }
 
-void accountStatus(account acc) {
+void accountStatus(account acc) { /*When this is called, it prints out the status of the current account*/
 	title();
 	printf("ACCOUNT STATUS\n");
 	printf("**************\n\n");
@@ -28,7 +28,7 @@ void accountStatus(account acc) {
 	getchar();
 }
 
-account accountCreate(account acc) {
+account accountCreate(account acc) { /*Creates a account in accordance with the next available account number*/
 	printf("ACCOUNT CREATION\n");
 	printf("****************\n\n");
 	printf("What is your name?\n#: ");	
@@ -40,7 +40,7 @@ account accountCreate(account acc) {
 	return acc;
 }
 
-account withdraw(account acc) {
+account withdraw(account acc) { /*Function for withdrawing money*/
 	float amount;
 	char retry;
 	char status;
@@ -50,7 +50,7 @@ account withdraw(account acc) {
 	printf("Your current balance is: $%.02f\n\n", acc.balance);
 	printf("How much would you like to withdraw?\n#: ");
 	scanf("%f", &amount);
-	if (amount > acc.balance) {
+	if (amount > acc.balance) { /*Checks if requested withdrawal amount is greater than what is in the account*/
 		printf("\nSorry, you do not have enough funds to withdraw $%0.2f\n", amount);
 		printf("\nRetry? [y/n]\n#: ");
 		getchar();
@@ -76,7 +76,7 @@ account withdraw(account acc) {
 	}
 }
 
-account deposit(account acc) {
+account deposit(account acc) { /*Deposit money function*/
 	float amount;
 	char status;
 	title();
@@ -99,58 +99,60 @@ account deposit(account acc) {
 		return acc;
 }
 
-void mainMenu(account acc[]) {
-	int menu = 1; //1:MAIN, 2:CREATE, 3:MANAGE
+void mainMenu(account acc[]) { /*Menu System*/
+	int menu = 1; /*1:MAIN, 2:CREATE, 3:MANAGE*/
 	int option;
-	int account = -1;
+	int account = -1; /*Keeps track of the current user*/
 	do {
 		title();
-		switch(menu){
-			case 1:
+		switch(menu){ /*System for displaying the correct menu*/
+			case 1: /*Main Menu*/
 				printf("How may I help you today?\n\n1: Create A New Account\n2: Manage An Existing Account\n3: Exit\n\n#: ");
 				scanf("%d", &option);
 				switch(option) {
-					case 1: //Create
+					case 1: /*Create*/
 						menu = 2;
 						break;
-					case 2: //Manage
+					case 2: /*Manage*/
 						menu = 3;
 						break;
-					case 3: //Exit
+					case 3: /*Exit*/
 						menu = 0;
 						break;
 					default: menu = 1;
 				}
 				break;
-			case 2:
+			case 2: /*Create a new account*/
 				acc[a] = accountCreate(acc[a]);
 				a++;
 				menu = 1;
 				break;
-			case 3:
-				if (account == -1) {
+			case 3: /*Manage an existing account*/
+				if (account == -1) { /*Asks the user to input their account number*/
 					printf("Please enter your account number\n#: ");
 					scanf("%d", &account);
 					break;
 					}
-				else {
-					printf("Hello %s", acc[account].name);
+				else { /*For if the user has already logged in*/
+					printf("Hello %s\n", acc[account].name);
+					printf("Account Number %d\n\n", acc[account].number);
 					printf("What action would you like to perform?\n\n1: Witdraw\n2: Deposit\n3: Check Balance\n4: Return\n#: ");
 					scanf("%d", &option);
 					getchar();	
 					switch(option) {
-						case 1: //Withdraw
+						case 1: /*Withdraw*/
 							acc[account] = withdraw(acc[account]);
 							break;
-						case 2: //Deposit
+						case 2: /*Deposit*/
 							acc[account] = deposit(acc[account]);
 							break;
-						case 3: //Status Check
+						case 3: /*Status Check*/
+							title();
 							printf("Your current balance is: $%.02f", acc[account].balance);
 							printf("\nPress Enter to Return.");
 							getchar();
 							break;
-						case 4:
+						case 4: /*Logs the user out and resets the current account in use*/
 							menu = 1;
 							account = -1;
 							break;
@@ -165,7 +167,9 @@ void mainMenu(account acc[]) {
 
 void initialize() {
 	int n;
-	printf("Input number of total accounts #: ");
+	printf("\nADMINISTRATION\n");
+	printf("**************\n");
+	printf("Input number of total accounts #: "); /*Prompts the user before the bank function starts to input the allowed amount bank acounts*/
 	scanf("%d", &n);
 	account acc[n];
 
